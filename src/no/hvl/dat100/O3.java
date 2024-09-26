1761b8ada6d69a7eb4dc9c1752d92ce0e2556c21
amss04 <alina.syeda2004@gmail.com>
2024-09-26 14:50:49 +0200

    Create O3.java

---
+++ src/no/hvl/dat100/O3.java
@@ -0,0 +1,13 @@
+package no.hvl.dat100;
+ 
+	import static javax.swing.JOptionPane.*;
+	import static java.lang.Integer.parseInt;
+
+ 
+	public class = {
+ 
+	
+			public static void main(String[] args) {
+			        	String tallInput = showInputDialog("Skriv inn et heltall n > 0 for å beregne n!");
+
+			        	int tall = Integer.parseInt(tallInput);
